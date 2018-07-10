c3dca32f8da80e4e3b422fc837204de1e13b4393
Alexandre Piel <alexandre.piel@gmail.com>
2018-07-10 21:30:22 +0200

    addddddd

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,12 @@ class DeviceDiscoverer {
   StreamController _clientController =
     new StreamController.broadcast();
 
+  List log;
+
+  DeviceDiscoverer(List l) {
+    this.log = l;
+  }
+
   Future start() async {
     _socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4.address, 0);
 
@@ -28,6 +34,7 @@ class DeviceDiscoverer {
 
           var data = utf8.decode(packet.data);
           print(data);
+          this.log.insert(0, data);
 
           break;
       }
@@ -51,6 +58,7 @@ class DeviceDiscoverer {
   Stream get clients => _clientController.stream;
 
   void search([String searchTarget]) {
+    this.log.insert(0, '#### send search');
     print('send search');
     if (searchTarget == null) {
       searchTarget = "upnp:rootdevice";
@@ -71,7 +79,7 @@ class DeviceDiscoverer {
 
   Timer _discoverySearchTimer;
 
-  Stream quickDiscoverClients() async* {
+  Future quickDiscoverClients() async {
     if (_socket == null) {
       await start();
       await new Future.delayed(const Duration(seconds: 1));
@@ -81,27 +89,23 @@ class DeviceDiscoverer {
     new Future.delayed(new Duration(seconds: 10), () {
       stop();
     });
-
-    yield* clients;
   }
 }
 
-void main() async {
+void main() {
   print('helllloooo');
-  var disc = new DeviceDiscoverer();
-  // disc.quickDiscoverClients().listen((client) async {
-  //   print('client');
-  //   print(client);
-  // });
-
-  runApp(new MyApp(disc));
+  List log = ['Upnp discovery...'];
+  var disc = new DeviceDiscoverer(log);
+  runApp(new MyApp(disc, log));
 }
 
 class MyApp extends StatelessWidget {
   DeviceDiscoverer discoverer;
+  List log;
 
-  MyApp(DeviceDiscoverer d) {
+  MyApp(DeviceDiscoverer d, List l) {
     this.discoverer = d;
+    this.log = l;
   }
 
   // This widget is the root of your application.
@@ -111,19 +115,12 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(
         title: 'Flutter Demo Home Page',
         discoverer: discoverer,
+        log: log,
       ),
     );
   }
@@ -131,16 +128,8 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   final DeviceDiscoverer discoverer;
-  MyHomePage({Key key, this.title, this.discoverer}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  final List log;
+  MyHomePage({Key key, this.title, this.discoverer, this.log}) : super(key: key);
 
   final String title;
 
@@ -149,64 +138,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    // widget.discoverer
-    widget.discoverer.quickDiscoverClients().listen((client) async {
-      print('client');
-      print(client);
-    });
+  Future _discover() async {
+    await widget.discoverer.quickDiscoverClients();
+    setState(() {});
+  }
+
+  List<Text> _buildList() {
+    return widget.log == null ? []
+         : widget.log.map((value) => new Text(value)).toList();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed yo the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: new ListView(
+        reverse: true,
+        children: _buildList(),
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _discover,
+        tooltip: 'Discover',
         child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
