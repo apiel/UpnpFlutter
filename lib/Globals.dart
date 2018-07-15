d408186264d3ae05d8cc5d0fa2534a60dc0c1196
Alexandre Piel <alexandre.piel@gmail.com>
2018-07-15 22:51:53 +0200

    logloglog

--- lib/Globals.dart
+++ lib/Globals.dart
@@ -1,6 +1,16 @@
 
 import './services/DeviceDiscoverer.dart';
 
+class LogItem {
+  String type = 'default';
+  String value;
+
+  LogItem(String value, { type: 'default' }) {
+    this.type = type;
+    this.value = value;
+  }
+}
+
 class Globals {
   static final Globals _singleton = new Globals._internal();
   DeviceDiscoverer discoverer;
@@ -11,6 +21,7 @@ class Globals {
   }
 
   Globals._internal() {
+    // this.log = [new LogItem('Upnp discovery...')];
     this.log = ['Upnp discovery...'];
     this.discoverer = new DeviceDiscoverer(this.log);
   }
--- lib/screens/DevicesScreen/DevicesScreen.dart
+++ lib/screens/DevicesScreen/DevicesScreen.dart
@@ -10,6 +10,26 @@ class DevicesScreen extends StatelessWidget {
         title: new Text('Devices'),
         context: context,
       ),
+      body: new CustomScrollView(
+        primary: false,
+        slivers: <Widget>[
+          new SliverPadding(
+            padding: const EdgeInsets.all(20.0),
+            sliver: new SliverGrid.count(
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: <Widget>[
+                new Card(child: new Text('He\'d have you all unravel at the')),
+                new Card(child: new Text('Heed not the rabble')),
+                new Card(child: new Text('Sound of screams but the')),
+                new Card(child: new Text('Who scream')),
+                new Card(child: new Text('Revolution is coming...')),
+                new Card(child: new Text('Revolution, they...')),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
