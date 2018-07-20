381839fffdb4c67367620121fe217b2b4a2dc158
Alexandre Piel <alexandre.piel@gmail.com>
2018-07-20 21:09:17 +0200

    stufffffff

--- lib/widgets/DeviceWidget.dart
+++ lib/widgets/DeviceWidget.dart
@@ -2,17 +2,34 @@ import 'package:flutter/material.dart';
 
 import '../models/Device.dart';
 
-class DeviceWidget extends StatelessWidget {
+class DeviceWidget extends StatefulWidget {
   final Device device;
 
-  DeviceWidget(this.device);
+  DeviceWidget(this.device, { Key key }) : super(key: key);
+
+  @override
+  DeviceWidgetState createState() =>
+      new DeviceWidgetState(this.device, ['TOGGLE', 'ON', 'OFF']);
+}
+
+class DeviceWidgetState extends State<DeviceWidget> {
+  final Device device;
+  List<String> values;
+  int currentValue = 0;
+
+  DeviceWidgetState(this.device, this.values);
 
   @override
   Widget build(BuildContext context) {
     return new Card(
       child: new GestureDetector(
         onTap: () { print('tap tap'); },
-        onHorizontalDragUpdate: (yo) { print('drag'); print(yo); },
+        onHorizontalDragEnd: (drag) {
+          setState(() {
+            int direction = drag.velocity.pixelsPerSecond.dx > 0 ? 1 : -1;
+            this.currentValue = (this.currentValue + direction) % this.values.length;
+          });
+        },
         child: new Container(
           color: Colors.black12, // without this it doesnt get fullsize
           child: new Column(
@@ -21,56 +38,11 @@ class DeviceWidget extends StatelessWidget {
             children: <Widget>[
               new Text(this.device.name),
               new Icon(Icons.wb_incandescent),
-              new Text('ON'),
+              new Text(this.values[this.currentValue]),
             ],
           ),
         ),
       ),
-
-
-      // child: new FlatButton(
-      //   child: new Column(
-      //     mainAxisSize: MainAxisSize.min,
-      //     children: <Widget>[
-      //       new Text(this.device.name),
-      //       new Icon(Icons.wb_incandescent),
-      //       new Text('ON'),
-      //     ],
-      //   ),
-      //   onPressed: () { /* ... */ },
-      // ),
-
-
-      // child: new FlatButton(
-
-      //   child: new ListTile(
-      //     leading: const Icon(Icons.bug_report),
-      //     title: new Text(this.device.name),
-      //     subtitle: new Text('on'),
-      //   ),
-      //   onPressed: () { /* ... */ },
-      // ),
-
-      // child: new Column(
-      //   mainAxisSize: MainAxisSize.min,
-      //   children: <Widget>[
-      //     new ListTile(
-      //       title: new Text(this.device.name),
-      //     ),
-      //     new FlatButton(
-      //       child: const Text('ON'),
-      //       onPressed: () { /* ... */ },
-      //     ),
-      //   ],
-      // ),
-
-
-
-      // child: new Row(
-      //   children: <Widget>[
-      //     new Text(this.device.name)
-      //   ],
-      // ),
     );
   }
 }
