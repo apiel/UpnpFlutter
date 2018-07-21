55b0aded4c606ce6f3003caa399976d2a43c3a32
Alexandre Piel <alexandre.piel@gmail.com>
2018-07-21 23:07:10 +0200

    stuff yo socket

--- lib/services/DeviceService.dart
+++ lib/services/DeviceService.dart
@@ -1,7 +1,7 @@
-// import 'dart:io';
+import 'dart:io';
 import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
 
 import '../Globals.dart';
 import '../models/LogItem.dart';
@@ -20,11 +20,26 @@ class DeviceService {
   Future<String> callUrl(String url, String data) async {
     this.globals.log.insert(0, new LogItem(url, type: 'call url'));
     try {
-      http.Response response = await http.post(url, body: data);
-      // print('yoooo response');
-      // print(response);
-      this.globals.log.insert(0, new LogItem(response.body, type: 'parse body'));
-      return response.body;
+      // not working i dont know why
+      // http.Response response =
+      //   await http.post(
+      //     url,
+      //     body: data,
+      //   );
+      // this.globals.log.insert(0, new LogItem(response.body, type: 'parse body'));
+      // return response.body;
+
+      Socket socket = await Socket.connect('192.168.0.66', 80);
+      print('connected');
+      String body = """POST /api/S6QJ3NqpQzsR6ZFzOBgxSRJPW58C061um8oP8uhf/lights/MHB_ROOM00/state HTTP/1.1
+content-type: application/json; charset=utf-8
+accept-encoding: gzip
+content-length: 13
+host: 192.168.0.66
+
+$data\n""";
+      socket.write(body);
+      socket.close();
     } catch (e) {
       this.globals.log.insert(0, new LogItem(e.toString(), type: '!error post'));
     }
@@ -32,9 +47,10 @@ class DeviceService {
   }
 
   Future<String> send(String action) async {
-    String url = '${this.device.url}/S6QJ3NqpQzsR6ZFzOBgxSRJPW58C061um8oP8uhf/lights/state';
+    String url = '${this.device.url}/S6QJ3NqpQzsR6ZFzOBgxSRJPW58C061um8oP8uhf/lights/${this.device.uid}/state';
+    // String url = '${this.device.url}/S6QJ3NqpQzsR6ZFzOBgxSRJPW58C061um8oP8uhf/lights/state';
     String newState = action == 'ON' || (action == 'TOGGLE' && this.device.on == false) ? 'true' : 'false';
-    String data = '{"on": $newState}';
+    String data = '{"on": $newState}\n';
     print('callllllll (${this.device.on}): $url -> $data');
     String jsonString = await callUrl(url, data);
     // Map decoded = json.decode(jsonString);
--- yo.dart
+++ yo.dart
@@ -1,14 +1,28 @@
-void main() {
-          RegExp regExp = new RegExp(r"LOCATION: *(.*)\s", caseSensitive: false);
-          var match = regExp.firstMatch("""HTTP/1.1 200 OK
-Cache-Control: max-age=120
-EXT:
-Location: http://192.168.0.1:65535/rootDesc.xml
-Server: Linux/2.4.22-1.2115.nptl UPnP/1.0 miniupnpd/1.0
-ST: upnp:rootdevice
-USN: uuid:add0d0d8-1dd1-11b2-a3c3-c36ac1b1c169::upnp:rootdevice""");
-          if (match != null) {
-            print(match[1]);
-          }
+import 'package:http/http.dart' as http;
+// import 'dart:io';
+import 'dart:async';
+
+Future main() async {
+//           RegExp regExp = new RegExp(r"LOCATION: *(.*)\s", caseSensitive: false);
+//           var match = regExp.firstMatch("""HTTP/1.1 200 OK
+// Cache-Control: max-age=120
+// EXT:
+// Location: http://192.168.0.1:65535/rootDesc.xml
+// Server: Linux/2.4.22-1.2115.nptl UPnP/1.0 miniupnpd/1.0
+// ST: upnp:rootdevice
+// USN: uuid:add0d0d8-1dd1-11b2-a3c3-c36ac1b1c169::upnp:rootdevice""");
+//           if (match != null) {
+//             print(match[1]);
+//           }
+
+  String url = 'http://192.268.0.66/S6QJ3NqpQzsR6ZFzOBgxSRJPW58C061um8oP8uhf/lights/state';
+  String data = '{"on": true}';
+  http.Response response =
+    await
+    http.post(
+      url,
+      body: data,
+    );
+  print(response);
 }
 
